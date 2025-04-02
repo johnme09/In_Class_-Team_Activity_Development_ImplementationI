e8a84ff8c579aee5afd28b9fb579902c789a772e
Hayden Jones <jonehm05@pfw.edu>
2025-04-02 16:45:50 -0400

    Added main, authenticator, and Stage

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+
+public class Main {    
+    private static Authenticator auth;
+    private String currentUser;
+    private String currentRole;
+
+    public static void main(String[] args) {        
+        // Initialize the Authenticator
+        auth = new Authenticator();
+        
+    }  
+    
+    private void login() {
+    
+    }
+
+    private void logout() {
+
+    }
+
+    private boolean checkAuth() {
+        return false;
+    }
+
+    private void enrollRegister() {
+
+    } 
+
+    private void toggleRole() {
+
+    }
+
+    private void answerSubmit() {
+
+    }
+
+    private double scoreQuestion(int answer) {
+        return 0.0;
+    }
+
+    private LinkedList<CourseRecord> pullClassReport() {
+        return null;
+    }
+
+    private void updateVisuals() {
+
+    }
+
+    private void setLayoutOwner() {
+
+    }
+
+    private void setLayoutLearner() {
+
+    }
+}
